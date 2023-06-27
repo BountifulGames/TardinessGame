e1c79206d7a8f5e3c80034c5dbdbb7ada67aa3f7
macmay91 <macmay91@gmail.com>
2023-06-27 12:15:11 -0400

    Rough pass at Final Score
    And misc fixes

--- Assets/Scripts/UI/WinScreen.cs
+++ Assets/Scripts/UI/WinScreen.cs
@@ -6,12 +6,29 @@ using TMPro;
 
 public class WinScreen : MonoBehaviour
 {
+    [Header("Text References")]
     public TMP_Text timeText;
     public TMP_Text gradeText;
+    public TMP_Text extraCredit;
+    public TMP_Text finalScore;
+    private float totalMinutes;
+    private float totalSeconds;
+    private float gradeAverage;
+    private float bonus;
 
-    private void Awake()
+    [Header("Times to beat")]
+    public float hiTime;
+    public float midTime;
+    public float lowTime;
+
+    private void Start()
     {
+        //combine scores from level one and two
         CalculateScore();
+        //check time for extra credit bonus
+        CalculateExtraCredit();
+        //combine score + extra credit for final score
+        FinalScore();
 
     }
 
@@ -32,18 +49,47 @@ public class WinScreen : MonoBehaviour
         //remainder of seconds
         float seconds2 = Mathf.FloorToInt(LevelTwo % 60);
 
-        float totalMinutes = Mathf.FloorToInt((minutes1 + minutes2) + (seconds1 + seconds2) / 60);
+        totalMinutes = Mathf.FloorToInt((minutes1 + minutes2) + (seconds1 + seconds2) / 60);
         Debug.Log(totalMinutes + "total minutes");
-        float totalSeconds = Mathf.FloorToInt((seconds1 + seconds2) % 60);
+        totalSeconds = Mathf.FloorToInt((seconds1 + seconds2) % 60);
         Debug.Log(totalSeconds + "total seconds");
 
         //display time text with proper formatting
-        timeText.text = string.Format("{0:00}:{1:00}", minutes1, seconds1) + "<br>" + string.Format("{0:00}:{1:00}", minutes2, seconds2) + "<br>" + string.Format("{0:00}:{1:00}", totalMinutes, totalSeconds);
+        timeText.text = "Stage 1: " + string.Format("{0:00}:{1:00}", minutes1, seconds1) + "<br>" + "Stage 2: " + string.Format("{0:00}:{1:00}", minutes2, seconds2) + "<br>" + "Total: " + string.Format("{0:00}:{1:00}", totalMinutes, totalSeconds);
 
-        float gradeAverage = (GameManager.gm.levelOneGrade + GameManager.gm.levelTwoGrade) / 2;
+        gradeAverage = (GameManager.gm.levelOneGrade + GameManager.gm.levelTwoGrade) / 2;
 
         //displays grade values
-        gradeText.text = GameManager.gm.levelOneGrade + "<br>" + GameManager.gm.levelTwoGrade + "<br>" + gradeAverage;
+        gradeText.text = "Stage 1: " + GameManager.gm.levelOneGrade + "<br>" + "Stage 2: " + GameManager.gm.levelTwoGrade + "<br>" + "Average: " + gradeAverage;
+    }
+
+    private void CalculateExtraCredit()
+    {
+        float rawTime = (totalMinutes * 60) + totalSeconds;
+        if(rawTime >= hiTime)
+        {
+            bonus = 3;
+        }
+        else if(rawTime >= midTime)
+        {
+            bonus = 2;
+        }
+        else if(rawTime >= lowTime)
+        {
+            bonus = 1;
+        }
+        else
+        {
+            bonus = 0;
+        }
+        extraCredit.text = "+ " + bonus;
+
+    }
+
+    private void FinalScore()
+    {
+        float finalScoreValue = gradeAverage + bonus;
+        finalScore.text = "" + finalScoreValue;
     }
     public void MainMenu()
     {
